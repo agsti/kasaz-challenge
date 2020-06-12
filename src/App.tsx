7aa4aad91bba13e69e2726722c98a89ca5a941d2
Agusti Bau <agustibau@gmail.com>
2020-06-13 00:42:30 +0200

    added ListingsList

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,39 @@ import React, { Component} from "react";
 import { hot } from "react-hot-loader/root";
 import HeaderBar from "./components/HeaderBar";
 import SearchBar from "./components/SearchBar";
-import FilterView from "./components/FilterView";
+//import FilterView from "./components/FilterView";
+import ListingsList from "./components/ListingsList";
 
 import "./App.css";
 import 'reset-css';
 
+const l = {
+    title: "Piso exterior y en buen estado con ascensor en Sant Marti",
+    price: 280000,
+    priceSqm: 2.718,
+    sqm: 103,
+    nRooms: 2,
+    nBath: 1,
+    picture: "https://dywf8esi8sang.cloudfront.net/pictures/028b300f1420101c1af71fde6e0fa7f4_large.jpg"
+}
 class App extends Component{
   render(){
     return(
         <div className="App">
             <HeaderBar />
             <SearchBar />
+{ /*
             <FilterView 
                 nRooms={["1+", "2+", "3+", "4+", "5+"]}
                 prices={["1","2","3","4"]}
                 propSizes={["30", "40", "50"]}
+             />
+*/}
+			<ListingsList
+				listings = {[l, l, l]}
+				/>
+	
 
-                    
-                    />
         </div>
     );
   }
---
+++ src/components/ListingsList/index.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import BathroomIcon from "./icons/bathroom.svg";
+import BedroomIcon from "./icons/bedroom.svg";
+import OkIcon from "./icons/ok.svg";
+
+import "./ListingsList.css";
+
+type ListingItemProps = {
+    title: string,
+    price: number,
+    priceSqm: number,
+    sqm: number,
+    nRooms: number,
+    nBath: number,
+    picture: string
+}
+
+
+const ListingItem = (props : ListingItemProps) => {
+    const {title, price, priceSqm, sqm, nRooms, nBath, picture} = props;
+    return (
+        <div className="listing-container">
+            <div className="listing-image-price">
+                <img src={picture} />
+                <div className="listing-prices">
+                    <div className="listing-price">
+                        {price.toLocaleString("es")} €
+                     </div>
+                    <div className="listing-price-sqm">
+                        {priceSqm} €/m²
+                     </div>
+                </div>
+            </div>
+
+            <div className="listing-title">
+                {title}
+            </div>
+            <div className="listing-info-container">
+                <div className="listing-info">
+					<img src={OkIcon} />
+                    {sqm} m²
+                </div>
+                <div className="listing-info">
+					<img src={BedroomIcon} />
+                    {nRooms} habs.
+                </div>
+                <div className="listing-info">
+					<img src={BathroomIcon} />
+                    {nBath} banos
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+type ListingListProps = {
+    listings: ListingItemProps[]
+}
+
+export default function ListingsList(props : ListingListProps){
+    const {listings} = props;
+    return (
+        <div className="listing-list">
+        {
+            listings.map((l, i) => (
+                                <ListingItem
+                                key={i}
+                                {...l}/>
+                                ))
+        }
+        </div>
+     )
+}
