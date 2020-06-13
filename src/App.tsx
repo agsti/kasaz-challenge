349d7560ef88178f89d9bb538c2ab4f4f05ca149
Agusti Bau <agustibau@gmail.com>
2020-06-13 16:49:00 +0200

    moved FilterView to FilterMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import {useSelector, useDispatch} from "react-redux";
+import {useSelector} from "react-redux";
 import { hot } from "react-hot-loader/root";
 
 import HeaderBar from "./components/HeaderBar";
 import SearchBar from "./components/SearchBar";
-import FilterView from "./components/FilterView";
+import FilterView from "./components/FilterMenu";
 import ListingsList from "./components/ListingsList";
 
 import {SelectMenuState} from "./redux/Menus/selectors";
-import {ToggleFilterMenu} from "./redux/Menus/actions";
 
 import "./App.css";
 import 'reset-css';
@@ -25,13 +24,11 @@ const l = {
 
 const App = () => {
     const menuState = useSelector(SelectMenuState)    
-    const dispatch = useDispatch();
-    console.log(menuState)
+
     return(
         <div className="App">
             <HeaderBar />
-            <SearchBar
-        />
+            <SearchBar />
         { 
             menuState.showFilterMenu && <FilterView 
                 nRooms={["1+", "2+", "3+", "4+", "5+"]}
