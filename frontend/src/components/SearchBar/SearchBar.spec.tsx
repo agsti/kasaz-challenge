281925acad284f4447f776ef142fdfe2f7d2fadd
Agusti Bau <agustibau@gmail.com>
2020-06-15 16:26:17 +0200

    Completed SearchBarView test

--- frontend/src/components/SearchBar/SearchBar.spec.tsx
+++ frontend/src/components/SearchBar/SearchBar.spec.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import { mount, shallow} from 'enzyme';
+import {shallow} from 'enzyme';
 import {describe, expect, it} from '@jest/globals';
 
-import SearchBar from './index';
+import SearchBarView from './SearchBarView';
 
 describe('SearchBar', () => {
-  it('should render correctly in "debug" mode', () => {
-      const component = shallow(<SearchBar />);
-  
+
+
+
+
+  it('should render correctly with ArrowLooking Up', () => {
+    const component = shallow(<SearchBarView
+      filterArrowLookingUp={true}
+      onFilterButtonClicked={jest.fn()}
+      onSearch={jest.fn()}
+      searchHasChanged={false}
+      searchValue="search"
+      setSearchValue={jest.fn()}
+    />);
     expect(component).toMatchSnapshot();
   });
+
+  it('should render correctly with ArrowLooking Down', () => {
+    const component = shallow(<SearchBarView
+      filterArrowLookingUp={false}
+      onFilterButtonClicked={jest.fn()}
+      onSearch={jest.fn()}
+      searchHasChanged={false}
+      searchValue="search"
+      setSearchValue={jest.fn()}
+    />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('fires FilterClick event', () =>{
+  const onFilterClicked = jest.fn();
+
+    const component = shallow(<SearchBarView
+        filterArrowLookingUp={false}
+        onFilterButtonClicked={onFilterClicked}
+        onSearch={jest.fn()}
+        searchHasChanged={false}
+        searchValue="search"
+        setSearchValue={jest.fn()}
+      />);
+      component.find(".filter-btn").simulate("click")
+      expect(onFilterClicked).toHaveBeenCalledTimes(1)
+  })
 });
